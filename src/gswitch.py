4c2ee3a0224d0f927efe9d06bc8453935e6efc22
thatswhereurwrongkiddo <noah.trizzle@gmail.com>
2019-03-21 21:08:41 -0400

    added gswitch.py for theme initialization/reinitialization

---
+++ src/gswitch.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+import os
+
+list = []
+root = "."
+for item in os.listdir(root):
+    if os.path.isdir(os.path.join(root, item)):
+        list.append(item)
+
+amount = 0
+
+
+for x in range(len(list)):
+    print("({0}) ".format(x+1) + list[x])
+
+for i in list:
+    amount = amount + 1
+
+print()
+print("Which would you like to be your next theme?")
+user_choice = input("Type (random) for a random one: ")
